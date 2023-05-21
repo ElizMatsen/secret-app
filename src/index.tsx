6e85e4671e8113d056412fb05c383dec23d0f714
liza_matsenka <macenko1996@gmail.com>
2023-05-21 15:50:57 +0300

    improved packages

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import store from "./app/store";
 import {Provider} from "react-redux";
-import 'react-toastify/dist/ReactToastify.css';
-import {ToastContainer} from "react-toastify";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -20,7 +18,6 @@ root.render(
                 <App/>
             </BrowserRouter>
         </Provider>
-        <ToastContainer/>
     </React.StrictMode>
 );
 
